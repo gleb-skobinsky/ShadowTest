71475340fed60a3031fb19a092f2104252a3c138
g.gutnik <gleb.g@itechpsp.com>
2024-07-23 12:00:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/shadowtest/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.shadowtest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.shadowtest.ui.theme.ShadowTestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ShadowTestTheme {
+                Box(
+                    Modifier
+                        .shadow(
+                            elevation = 8.dp,
+                            spotColor = Color.Yellow,
+                            ambientColor = Color.Yellow
+                        )
+                        .size(80.dp, 36.dp)
+                ) {
+                    Text("Hello world")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ShadowTestTheme {
+        Greeting("Android")
+    }
+}
